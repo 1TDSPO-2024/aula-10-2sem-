05042356e8c76f5ff365287f2c2dfcf2cadc6b2d
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2024-09-11 22:34:45 -0300

    Utilizando hooks, tipos e localStorage, filtro de busca e apresentação de dados.

--- aula10/src/routes/EditarProdutos/index.tsx
+++ aula10/src/routes/EditarProdutos/index.tsx
@@ -1,14 +1,34 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { TipoProduto } from "../../types";
 
 export default function EditarProdutos(){
 
     const {id} = useParams();
 
+    //Recuperando a lista do localStorage
+    const listaDeProdutosString = localStorage.getItem('lista') || '[]';
+    const lista:TipoProduto[] = JSON.parse(listaDeProdutosString);
+
+    const [produto, setProduto] = useState<TipoProduto>({
+        id: 0,
+        nome: '',
+        preco: 0.0,
+    });
+
+    useEffect(() => {
+      const produtoEncontrado = lista.find(p => p.id === Number(id));
+      if(produtoEncontrado)
+        setProduto(produtoEncontrado);
+
+    }, [])
+    
     return (
         <div>
             <h1>Editar Produtos</h1>
             <div>
                 <p>Parâmetro que veio do componente produto! : {id}</p>
+                <p>Nome: {produto.nome}</p>
             </div>
         </div>
     );
