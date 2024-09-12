e0c46ba706deda4ddb443892e7e0e01b9a307bad
SunaUezuri <wesley.jane.santos@gmail.com>
2024-09-11 22:51:09 -0300

    Método editar com handleSubmit criado.

--- aula10/src/routes/EditarProdutos/index.tsx
+++ aula10/src/routes/EditarProdutos/index.tsx
@@ -1,9 +1,10 @@
-import { useParams } from "react-router-dom";
-import { TipoProduto } from "../../types";
 import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { TipoProduto } from "../../types";
 
 export default function EditarProdutos(){
 
+    const navigate = useNavigate();
     const {id} = useParams();
 
     //Recuperando a lista do localStorage
@@ -17,18 +18,42 @@ export default function EditarProdutos(){
     });
 
     useEffect(() => {
-        const produtoEncontrado = lista.find(p => p.id === Number(id));
-        if(produtoEncontrado){
-            setProduto(produtoEncontrado);
-        }
-    })
+      const produtoEncontrado = lista.find(p => p.id === Number(id));
+      if(produtoEncontrado)
+        setProduto(produtoEncontrado);
+
+    }, [])
+    
+    const handleSubmit = (e:React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+
+      if(produto){
+        const idProduto = lista.findIndex(p => p.id === produto.id);
+        lista.splice(idProduto, 1, produto);
+        localStorage.setItem('lista', JSON.stringify(lista));
+        alert('Produto editado com sucesso!');
+        navigate("/produtos");
+      }
+    }
 
     return (
         <div>
             <h1>Editar Produtos</h1>
             <div>
-                <p>Parâmetro que veio do componente produto : {id}</p>
-                <p>Nome: {produto.nome}</p>
+                <p>Parâmetro que veio do componente produto! : {id}</p>
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        <label>Nome:</label>
+                        <input type="text" value={produto.nome} onChange={(e) => setProduto({...produto, nome: e.target.value})}/>
+                    </div>
+                    <div>
+                        <label>Preço:</label>
+                        <input type="number" value={produto.preco} onChange={(e) => setProduto({...produto, preco: parseInt(e.target.value)})}/>
+                    </div>
+                    <div>
+                        <button type="submit">Editar</button>
+                    </div>
+                </form>
             </div>
         </div>
     );
