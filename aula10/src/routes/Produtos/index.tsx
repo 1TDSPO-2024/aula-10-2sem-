8a2487fdd12c3b95388c9c1fe42c0d4e77d47378
SunaUezuri <wesley.jane.santos@gmail.com>
2024-09-11 21:51:42 -0300

    Lista de Produtos criada e iterada no componente.

--- aula10/src/routes/Produtos/index.tsx
+++ aula10/src/routes/Produtos/index.tsx
@@ -1,7 +1,37 @@
 export default function Produtos(){
+
+    const listaDeProdutos = [
+        {id: 1, nome: 'Camiseta', preco: 19.99},
+        {id: 2, nome: 'Calça', preco: 39.99},
+        {id: 3, nome: 'Tênis', preco: 99.99}
+    ];
+
     return (
         <div>
             <h1>Produtos</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>Preço</th>
+                        <th>Editar</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {listaDeProdutos.map((p)=>(
+                        <tr key={p.id}>
+                            <td>{p.nome}</td>
+                            <td>{p.preco}</td>
+                            <td><button>Editar</button></td>
+                        </tr>
+                    ))}
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <td colSpan={3}>Total: R$ {listaDeProdutos.reduce((ac, p) => ac + p.preco, 0)}</td>
+                    </tr>
+                </tfoot>
+            </table>
         </div>
     );
 }
