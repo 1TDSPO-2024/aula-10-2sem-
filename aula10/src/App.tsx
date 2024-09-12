25bda8a5f331ac5c3647b7d48fab0b429fefca5b
SunaUezuri <wesley.jane.santos@gmail.com>
2024-09-11 21:35:53 -0300

    Preparando o projeto com estrutura semântica.

--- aula10/src/App.tsx
+++ aula10/src/App.tsx
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom";
+import Cabecalho from "./components/Cabecalho/Cabecalho";
+import Rodape from "./components/Rodape/Rodape";
 
 export default function App(){
   return (
     <div>
         <h1>Usuário do Github</h1>
+        <Cabecalho/>
         <Outlet/>
+        <Rodape/>
     </div>
   );
 }
